4030a7aa3aaaa6cff36cfc8b49f531111c3f32e8
Saiguang Che <saiguang@alluxio.com>
2022-11-26 23:32:03 +0800

    create spring-boot project

---
+++ src/test/java/com/alluxio/alluxiomanager/AlluxioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alluxio.alluxiomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlluxioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
